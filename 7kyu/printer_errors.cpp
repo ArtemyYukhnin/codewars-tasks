dc73877e659eb7813ecf602d1618bb844115d5be
Artemy <artemyyukhnin7@gmail.com>
2026-07-14 22:14:35 +0500

    add

---
+++ 7kyu/printer_errors.cpp
@@ -0,0 +1,29 @@
+/*
+In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+
+The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+The string has a length greater or equal to one and contains only letters from ato z.
+Examples:
+
+s="aaabbbbhaijjjm"
+printer_error(s) => "0/14"
+
+s="aaaxbbbbyyhwawiwjjjwwm"
+printer_error(s) => "8/22"
+*/
+
+#include <algorithm>
+class Printer
+{
+public:
+    static std::string printerError(const std::string &s){
+      int all = s.length();
+      int miss = std::count_if(s.begin(), s.end(), [](char i){return i > 'm';});
+      return std::to_string(miss) + "/" + std::to_string(all);
+    }
+};
